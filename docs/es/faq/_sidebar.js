249f36d36837019539eb838ede1d744a3b0ea10d
muriplz <46825658+muriplz@users.noreply.github.com>
2024-04-16 14:07:05 +0200

    Fix route

--- docs/es/faq/_sidebar.js
+++ docs/es/faq/_sidebar.js
@@ -1,8 +1,8 @@
 module.exports = [
 	{
-		text: "Common Questions",
+		text: "Preguntas frecuentes",
 		icon: "faq",
-		prefix: "/faq/",
+		prefix: "/es/faq/",
 		collapsible: false,
 		children: ["", "Claim-Management", "Flag-Definitions", 
 		"Options", "Permissions", "plugin-comp"],
